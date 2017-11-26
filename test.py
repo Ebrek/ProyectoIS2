abef1291ec522144ce77d75bbec784c2e7caf959
Leo Wong <leowonglaw@gmail.com>
2017-11-25 20:55:13 -0500

    pantalla puntuacion v0.1

--- test.py
+++ test.py
@@ -1,27 +1,49 @@
 import pygame
 import sys
 import Resources.pygame_textinput
- 
+from constantes import *
 pygame.init()
 
-# Create TextInput-object
-textinput = Resources.pygame_textinput.TextInput(text_color=(255,255,255))
 
-screen = pygame.display.set_mode((1000, 200))
-clock = pygame.time.Clock()
+class Pantalla_Puntuacion():
 
-while True:
-    screen.fill((0, 0, 0))
+    def __init__(self ):
+        #self.bg=bg
+        # Create TextInput-object
+        self.textinput = Resources.pygame_textinput.TextInput(text_color=OLIVE)
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
 
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            exit()
+    def draw_text(self,text,size,color,x,y):
+        font = pygame.font.Font("freesansbold.ttf", size)
+        text_surface = font.render(text,True,color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
 
-    # Feed it with events every frame
-    textinput.update(events)
-    # Blit its surface onto the screen
-    screen.blit(textinput.get_surface(), (500, 100))
+    def mostrar(self):
+        while True:
+            self.screen.fill((0, 0, 0))
 
-    pygame.display.update()
-    clock.tick(30)
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    exit()
+                if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_RETURN:
+                        #some codes
+                        print(self.textinput.get_text())
+
+            # Feed it with events every frame
+            if len(self.textinput.get_text()) < 25 and pygame.K_DELETE in events:
+                self.textinput.update(events)
+            # Blit its surface onto the screen
+            self.screen.blit(self.textinput.get_surface(), (180, WIN_HEIGHT-45))
+
+
+            self.draw_text("Jugador:",32,WHITE,100,WIN_HEIGHT-50)
+            pygame.display.update()
+            self.clock.tick(30)
+
+b = Pantalla_Puntuacion()
+b.mostrar()
